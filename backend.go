292fbbc96ed8eeafc271eb1f59d91de09c9e3fbf
emersion <contact@emersion.fr>
2016-10-30 19:44:07 +0100

    Moves unlock stuff in a separate file

--- backend.go
+++ backend.go
@@ -1,41 +1,8 @@
 package pgp
 
 import (
-	"sync"
-
 	"github.com/emersion/go-imap/backend"
-
-	"golang.org/x/crypto/openpgp"
 )
-
-type UnlockFunction func(username, password string) (openpgp.EntityList, error)
-
-func UnlockRemember(f UnlockFunction) UnlockFunction {
-	cache := map[string]openpgp.EntityList{}
-	return func(username, password string) (openpgp.EntityList, error) {
-		if kr, ok := cache[username]; ok {
-			return kr, nil
-		}
-
-		kr, err := f(username, password)
-		if err != nil {
-			return nil, err
-		}
-
-		cache[username] = kr
-		return kr, nil
-	}
-}
-
-func UnlockSync(f UnlockFunction) UnlockFunction {
-	locker := &sync.Mutex{}
-	return func(username, password string) (openpgp.EntityList, error) {
-		locker.Lock()
-		defer locker.Unlock()
-
-		return f(username, password)
-	}
-}
 
 type Backend struct {
 	backend.Backend
